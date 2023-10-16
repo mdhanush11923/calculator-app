fa79075966bcd592581c73414c97d4278a37174f
M Dhanush <mdhanush11923@gmail.com>
2023-10-16 17:55:18 +0530

    f commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from './Calculator';
+
+function App() {
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
